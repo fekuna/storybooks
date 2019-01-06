b0679251b4122492d3104fcb98d79dc9b37fab6d
fekuna <hichirro@gmail.com>
2019-01-07 01:39:32 +0700

    second change

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("it works!");
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-})
+  console.log(`Server started on port ${port}`);
+});
